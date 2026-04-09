a40794898af49e19b7ef85e5c04050f9cdbc615c
odemain <oleksandrovsky@gmail.com>
2026-04-09 18:21:10 +0200

    - more global types

--- gamemaker-config/.ts/static/global.d.ts
+++ gamemaker-config/.ts/static/global.d.ts
@@ -1,10 +1,720 @@
-declare const mouse_x: Real;
-declare const mouse_y: Real;
-declare const application_surface: any;
-declare const fps_real: Real;
-declare const fps: Real;
-declare const view_camera: Array<Camera>;
-declare let room_height: Real;
-declare let room_width: Real;
-
-// todo: Complete these types (and all the others)
+/**
+ * None
+ */
+declare let font_texture_page_size: Real
+/**
+ * None
+ */
+declare let instance_count: Real
+/**
+ * None
+ */
+declare let instance_id: Array<Instance>
+/**
+ * None
+ */
+declare let alarm: Array<Real>
+/**
+ * None
+ */
+declare let depth: Real
+/**
+ * None
+ */
+declare let direction: Real
+/**
+ * None
+ */
+declare let friction: Real
+/**
+ * None
+ */
+declare let gravity: Real
+/**
+ * None
+ */
+declare let gravity_direction: Real
+/**
+ * None
+ */
+declare let hspeed: Real
+/**
+ * None
+ */
+declare let id: Instance
+/**
+ * None
+ */
+declare let layer: Layer
+/**
+ * None
+ */
+declare let on_ui_layer: Bool
+/**
+ * None
+ */
+declare let persistent: Bool
+/**
+ * None
+ */
+declare let solid: Bool
+/**
+ * None
+ */
+declare let speed: Real
+/**
+ * None
+ */
+declare let visible: Bool
+/**
+ * None
+ */
+declare let managed: Bool
+/**
+ * None
+ */
+declare let vspeed: Real
+/**
+ * None
+ */
+declare let x: Real
+/**
+ * None
+ */
+declare let xprevious: Real
+/**
+ * None
+ */
+declare let xstart: Real
+/**
+ * None
+ */
+declare let y: Real
+/**
+ * None
+ */
+declare let yprevious: Real
+/**
+ * None
+ */
+declare let ystart: Real
+/**
+ * None
+ */
+declare let object_index: GMObject
+/**
+ * None
+ */
+declare let event_number: EventNumber
+/**
+ * None
+ */
+declare let event_object: GMObject
+/**
+ * None
+ */
+declare let event_type: EventType
+/**
+ * None
+ */
+declare let path_endaction: PathAction
+/**
+ * None
+ */
+declare let path_index: GMPath
+/**
+ * None
+ */
+declare let path_orientation: Real
+/**
+ * None
+ */
+declare let path_position: Real
+/**
+ * None
+ */
+declare let path_positionprevious: Real
+/**
+ * None
+ */
+declare let path_scale: Real
+/**
+ * None
+ */
+declare let path_speed: Real
+/**
+ * None
+ */
+declare let room: GMRoom
+/**
+ * None
+ */
+declare let room_first: GMRoom
+/**
+ * None
+ */
+declare let room_height: Real
+/**
+ * None
+ */
+declare let room_last: GMRoom
+/**
+ * None
+ */
+declare let room_persistent: Bool
+/**
+ * None
+ */
+declare let room_width: Real
+/**
+ * None
+ */
+declare let in_sequence: Bool
+/**
+ * None
+ */
+declare let sequence_instance: SequenceInstance
+/**
+ * None
+ */
+declare let drawn_by_sequence: Bool
+/**
+ * None
+ */
+declare let bbox_bottom: Real
+/**
+ * None
+ */
+declare let bbox_left: Real
+/**
+ * None
+ */
+declare let bbox_right: Real
+/**
+ * None
+ */
+declare let bbox_top: Real
+/**
+ * None
+ */
+declare let image_alpha: Real
+/**
+ * None
+ */
+declare let image_angle: Real
+/**
+ * None
+ */
+declare let image_blend: Color
+/**
+ * None
+ */
+declare let image_index: Real
+/**
+ * None
+ */
+declare let image_number: Real
+/**
+ * None
+ */
+declare let image_speed: Real
+/**
+ * None
+ */
+declare let image_xscale: Real
+/**
+ * None
+ */
+declare let image_yscale: Real
+/**
+ * None
+ */
+declare let mask_index: GMSprite
+/**
+ * None
+ */
+declare let sprite_height: Real
+/**
+ * None
+ */
+declare let sprite_index: GMSprite
+/**
+ * None
+ */
+declare let sprite_width: Real
+/**
+ * None
+ */
+declare let sprite_xoffset: Real
+/**
+ * None
+ */
+declare let sprite_yoffset: Real
+/**
+ * None
+ */
+declare let timeline_index: GMTimeline
+/**
+ * None
+ */
+declare let timeline_loop: Bool
+/**
+ * None
+ */
+declare let timeline_position: Real
+/**
+ * None
+ */
+declare let timeline_running: Bool
+/**
+ * None
+ */
+declare let timeline_speed: Real
+/**
+ * None
+ */
+declare let view_camera: Array<Camera>
+/**
+ * None
+ */
+declare let view_current: Real
+/**
+ * None
+ */
+declare let view_enabled: Bool
+/**
+ * None
+ */
+declare let view_hport: Array<Real>
+/**
+ * None
+ */
+declare let view_surface_id: Array<Surface>
+/**
+ * None
+ */
+declare let view_visible: Array<Bool>
+/**
+ * None
+ */
+declare let view_wport: Array<Real>
+/**
+ * None
+ */
+declare let view_xport: Array<Real>
+/**
+ * None
+ */
+declare let view_yport: Array<Real>
+/**
+ * None
+ */
+declare let debug_mode: Bool
+/**
+ * None
+ */
+declare let fps: Real
+/**
+ * None
+ */
+declare let fps_real: Real
+/**
+ * None
+ */
+declare let application_surface: Surface
+/**
+ * None
+ */
+declare let program_directory: String
+/**
+ * None
+ */
+declare let temp_directory: String
+/**
+ * None
+ */
+declare let cache_directory: String
+/**
+ * None
+ */
+declare let working_directory: String
+/**
+ * None
+ */
+declare let event_data: DsMap
+/**
+ * None
+ */
+declare let keyboard_key: VirtualKey
+/**
+ * None
+ */
+declare let keyboard_lastchar: String
+/**
+ * None
+ */
+declare let keyboard_lastkey: VirtualKey
+/**
+ * None
+ */
+declare let keyboard_string: String
+/**
+ * None
+ */
+declare let mouse_button: MouseButton
+/**
+ * None
+ */
+declare let mouse_lastbutton: MouseButton
+/**
+ * None
+ */
+declare let mouse_x: Real
+/**
+ * None
+ */
+declare let mouse_y: Real
+/**
+ * None
+ */
+declare let cursor_sprite: GMSprite
+/**
+ * None
+ */
+declare let game_display_name: String
+/**
+ * None
+ */
+declare let game_id: Real
+/**
+ * None
+ */
+declare let game_project_name: String
+/**
+ * None
+ */
+declare let game_save_id: String
+/**
+ * None
+ */
+declare let current_day: Real
+/**
+ * None
+ */
+declare let current_hour: Real
+/**
+ * None
+ */
+declare let current_minute: Real
+/**
+ * None
+ */
+declare let current_month: Real
+/**
+ * None
+ */
+declare let current_second: Real
+/**
+ * None
+ */
+declare let current_time: Real
+/**
+ * None
+ */
+declare let current_weekday: Real
+/**
+ * None
+ */
+declare let current_year: Real
+/**
+ * None
+ */
+declare let delta_time: Real
+/**
+ * None
+ */
+declare let os_browser: BrowserType
+/**
+ * None
+ */
+declare let os_device: DeviceType
+/**
+ * None
+ */
+declare let os_type: OperatingSystem
+/**
+ * None
+ */
+declare let os_version: Real
+/**
+ * None
+ */
+declare let phy_active: Bool
+/**
+ * None
+ */
+declare let phy_angular_damping: Real
+/**
+ * None
+ */
+declare let phy_angular_velocity: Real
+/**
+ * None
+ */
+declare let phy_bullet: Bool
+/**
+ * None
+ */
+declare let phy_collision_points: Real
+/**
+ * None
+ */
+declare let phy_collision_x: Array<Real>
+/**
+ * None
+ */
+declare let phy_collision_y: Array<Real>
+/**
+ * None
+ */
+declare let phy_col_normal_x: Real
+/**
+ * None
+ */
+declare let phy_col_normal_y: Real
+/**
+ * None
+ */
+declare let phy_com_x: Real
+/**
+ * None
+ */
+declare let phy_com_y: Real
+/**
+ * None
+ */
+declare let phy_dynamic: Bool
+/**
+ * None
+ */
+declare let phy_fixed_rotation: Bool
+/**
+ * None
+ */
+declare let phy_inertia: Real
+/**
+ * None
+ */
+declare let phy_kinematic: Bool
+/**
+ * None
+ */
+declare let phy_linear_damping: Real
+/**
+ * None
+ */
+declare let phy_linear_velocity_x: Real
+/**
+ * None
+ */
+declare let phy_linear_velocity_y: Real
+/**
+ * None
+ */
+declare let phy_mass: Real
+/**
+ * None
+ */
+declare let phy_position_x: Real
+/**
+ * None
+ */
+declare let phy_position_xprevious: Real
+/**
+ * None
+ */
+declare let phy_position_y: Real
+/**
+ * None
+ */
+declare let phy_position_yprevious: Real
+/**
+ * None
+ */
+declare let phy_rotation: Real
+/**
+ * None
+ */
+declare let phy_sleeping: Bool
+/**
+ * None
+ */
+declare let phy_speed: Real
+/**
+ * None
+ */
+declare let phy_speed_x: Real
+/**
+ * None
+ */
+declare let phy_speed_y: Real
+/**
+ * None
+ */
+declare let browser_height: Real
+/**
+ * None
+ */
+declare let browser_width: Real
+/**
+ * None
+ */
+declare let webgl_enabled: Bool
+/**
+ * None
+ */
+declare let argument: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument0: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument1: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument2: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument3: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument4: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument5: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument6: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument7: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument8: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument9: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument10: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument11: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument12: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument13: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument14: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument15: ArgumentIdentity
+/**
+ * None
+ */
+declare let argument_count: Real
+/**
+ * None
+ */
+declare let in_collision_tree: Bool
+/**
+ * None
+ */
+declare let score: Real
+/**
+ * None
+ */
+declare let lives: Real
+/**
+ * None
+ */
+declare let health: Real
+/**
+ * None
+ */
+declare let display_aa: Real
+/**
+ * None
+ */
+declare let async_load: DsMap
+/**
+ * None
+ */
+declare let rollback_current_frame: Real
+/**
+ * None
+ */
+declare let rollback_confirmed_frame: Real
+/**
+ * None
+ */
+declare let rollback_event_id: Real
+/**
+ * None
+ */
+declare let rollback_event_param: GMObject
+/**
+ * None
+ */
+declare let rollback_game_running: Bool
+/**
+ * None
+ */
+declare let rollback_api_server: String
+/**
+ * None
+ */
+declare let player_id: Real
+/**
+ * None
+ */
+declare let player_local: Bool
+/**
+ * None
+ */
+declare let player_avatar_url: String
+/**
+ * None
+ */
+declare let player_avatar_sprite: GMSprite
+/**
+ * None
+ */
+declare let player_type: String
+/**
+ * None
+ */
+declare let player_user_id: String
+/**
+ * None
+ */
+declare let wallpaper_config: GMObject
